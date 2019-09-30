f2ce56f0e7d95919e7a7b53892f303338fd2f1ab
Santa Claus <joselynetusingwir@gmail.com>
2019-09-30 11:30:15 +0200

    files changed

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,7 +5,7 @@ from flask import render_template,redirect,url_for, flash,request
 from flask_login import login_user,logout_user,login_required
 from ..models import User
 from .forms import LoginForm,RegistrationForm
-from .. import db
+from ..import db,photos
 
 # from ..email import mail_message
 
